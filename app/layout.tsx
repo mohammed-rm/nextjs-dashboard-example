2a553112fd33eaf8a04853a1b05a6060948bc546
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2024-01-26 16:12:13 +0100

    fonts & sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css'
+import {raleway} from "@/app/ui/fonts";
+import React from "react";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${raleway.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Raleway, Lusitana} from 'next/font/google';
+
+export const raleway = Raleway({subsets: ["latin"]});
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']})
